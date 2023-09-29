36ac9bbb8baa2b97ae5bf4493d830d35dac7b2ec
DeeFlexman <129099006+DeeFlexman@users.noreply.github.com>
2023-09-29 10:44:06 +0200

    prototyping
    1st proto

---
+++ Assets/Scripts/Duanes_scripts/InterdimensionalTransport.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+public class InterdimensionalTransport : MonoBehaviour
+{
+    public GameObject ARCamera;
+
+    // Create an array of materials that you can assign in the Inspector.
+    public Material[] EnvironmentMaterials;
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider collider)
+    {
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(ARCamera.transform.position);
+
+        if (camPositionInPortalSpace.y < 1.0f)
+        {
+            // Disable Stencil test
+            for (int i = 0; i < EnvironmentMaterials.Length; ++i)
+            {
+                EnvironmentMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < EnvironmentMaterials.Length; ++i)
+            {
+                EnvironmentMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+    }
+}
+
