a5cc42c0b262621446c38b62b1518890c37e6f80
d3noDL <dino@adenogame.com>
2023-10-18 13:07:38 +0200

    Optimized the fireworks and added a skybox

--- Assets/Scripts/DinoScripts/PeaceController.cs
+++ Assets/Scripts/DinoScripts/PeaceController.cs
@@ -7,6 +7,7 @@ using Random = UnityEngine.Random;
 
 public class PeaceController : MonoBehaviour {
     [SerializeField] private GameObject _nuke;
+    [SerializeField] private GameObject _skyline;
     [SerializeField] private List<Transform> _nukeSpawnPoints;
     private bool shouldRun = false;
 
@@ -15,18 +16,19 @@ public class PeaceController : MonoBehaviour {
     
     public void Open() {
         shouldRun = true;
+        _skyline.SetActive(true);
         StartCoroutine(Tick());
     }
 
     public void Close() {
         shouldRun = false;
+        _skyline.SetActive(false);
         StopCoroutine(Tick());
     }
     
-    IEnumerator Tick() {
-        if (!shouldRun) yield return null;
 
-        while (true) {
+    IEnumerator Tick() {
+        while (shouldRun) {
             yield return new WaitForSeconds(Random.Range(1, 5));
             var randomSpawnNumber = Random.Range(
                 0, 
