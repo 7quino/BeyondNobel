112c3bc40657696c0c49fb5352df9b9cb57e2678
DeeFlexman <129099006+DeeFlexman@users.noreply.github.com>
2023-10-20 16:39:35 +0200

    whale moves
    hope this is my last push it real good

--- Assets/WhaleMovement.cs
+++ Assets/WhaleMovement.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class WhaleMovement : MonoBehaviour
 {
-    public Transform startPoint; // The starting point in world space
-    public Transform endPoint;   // The ending point in world space
-    public float moveDuration = 2f; // Time taken to move from start to end points (in seconds)
+    public float speed;
 
-    private float elapsedTime = 0f; // Time elapsed since the movement started
-
-    void Update()
+    void Start()
     {
-        // Increment the elapsed time based on the time passed since the last frame
-        elapsedTime += Time.deltaTime;
 
-        // Calculate the interpolation factor (t) between 0 and 1 based on elapsed time and move duration
-        float t = Mathf.Clamp01(elapsedTime / moveDuration);
-
-        // Use Vector3.Lerp to move the object smoothly from start to end points
-        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
-
-        // If the object has reached the end point, reset the elapsed time
-        if (t >= 1f)
-        {
-            elapsedTime = 0f;
-        }
     }
+
+    void Update() 
+    {
+        transform.Translate(0, 0, 1 * speed * Time.deltaTime);
+    }
+
+
+
 }
